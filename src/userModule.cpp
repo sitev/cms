34a002e29cab57ff8cf95389722e8254a0bd0755
mike <mike@sitev.ru>
2015-07-30 15:58:42 +0500

    userModule

---
+++ src/userModule.cpp
@@ -0,0 +1,281 @@
+#include "cjCms.h"
+
+namespace cj {
+
+//--------------------------------------------------------------------------------------------------
+//----------          class UserModule          ----------------------------------------------------
+//--------------------------------------------------------------------------------------------------
+
+UserModule::UserModule(SiteManager *manager) : WebModule(manager) {
+	setOptionsFromDB(9);
+}
+
+void UserModule::paint(WebPage *page, HttpRequest &request) {
+	String cmd = request.header.GET.getValue("cmd");
+	if (cmd == "ajax") 
+		return ajax(page, request);
+
+	if (page->site->login == "") page->out("login", "Login");
+	else page->out("login", page->site->login);
+
+	String p2 = request.header.GET.getValue("p2");
+
+	if (p2 == "") paintAbout(page, request);
+	else if (p2 == "recovery") paintRecovery(page, request);
+	else if (p2 == "signup") paintSignup(page, request);
+	else if (p2 == "sendAccount") sendAccount(page, request);
+	else if (p2 == "activate") activate(page, request);
+	else if (p2 == "changePassword") changePassword(page, request);
+	else paint404(page, request);
+}
+
+void UserModule::paintAbout(WebPage *page, HttpRequest &request) {
+	WebTemplate * tpl = new WebTemplate();
+	String userTpl = "user_tpl.html";
+	if (page->site->login == "") userTpl = "userNoEnter_tpl.html";
+
+	if (tpl->open(manager->modulePath + "/user/" + userTpl)) {
+		tpl->out("email", page->site->login);
+		tpl->out("money", "0");
+		tpl->exec();
+		page->out("content", tpl->html);
+	}
+}
+void UserModule::paintRecovery(WebPage *page, HttpRequest &request) {
+	WebTemplate * tpl = new WebTemplate();
+	String userTpl = "recovery_tpl.html";
+	if (tpl->open(manager->modulePath + "/user/" + userTpl)) {
+		tpl->exec();
+		page->out("content", tpl->html);
+	}
+}
+void UserModule::paintSignup(WebPage *page, HttpRequest &request) {
+	WebTemplate * tpl = new WebTemplate();
+	String userTpl = "signup_tpl.html";
+	if (tpl->open(manager->modulePath + "/user/" + userTpl)) {
+		tpl->exec();
+		page->out("content", tpl->html);
+	}
+}
+void UserModule::sendAccount(WebPage *page, HttpRequest &request) {
+	MySQL *query = manager->newQuery();
+	String guid = generateUUID();
+	String email = request.header.POST.getValue("email");
+	if (email != "") {
+		String password = manager->generateUserPassword();
+		String sql = "select * from users where email='" + email + "'";
+		if (query->exec(sql)) {
+			if (query->storeResult()) {
+				int count = query->getRowCount();
+				if (count > 0) {
+					guid = query->getFieldValue(0, "uuid");
+					sql = "update users set newPassword='" + password + "', uuid='" + guid + "' where email='" + email + "'";
+					if (query->exec(sql)) {}
+				}
+				else {
+					sql = "insert into users (email, newPassword, uuid) values('" + email + "', '" + password + "', '" + guid + "')";
+					if (query->exec(sql)) {}
+				}
+			}
+		}
+		string s = "Доброго времени суток!\n\nНа сайте " + page->site->host.toString8() +
+			" Вы сделали запрос на получение аккаунта и/или пароля .\n";
+		s += "Если это не так, просто удалите это письмо. \n\n";
+		s += "Ваш аккаунт: " + email.toString8() + "\n";
+		s += "Ваш пароль: " + password.toString8() + "\n\n";
+		s += "Для активизации Вашего аккаунта и/или пароля необходимо перейти по следующей ссылке:\n";
+		s += "http://" + page->site->host.toString8() + "/user/activate/" + guid.toString8() + "\n\n";
+
+		sendMail_s(email.toString8(), "noreply@" + page->site->host.toString8(), page->site->host.toString8() + ": подтверждение аккаунта", s);
+		WebTemplate * tpl = new WebTemplate();
+		if (tpl->open(manager->modulePath + "/user/loginSendAccount_tpl.html")) {
+			tpl->out("out", email);
+			tpl->exec();
+			page->out("content", tpl->html);
+		}
+	}
+}
+
+void UserModule::activate(WebPage *page, HttpRequest &request) {
+	MySQL *query = manager->newQuery();
+	String p3 = request.header.GET.getValue("p3");
+	String sql = "update users set active = '1', password=newPassword where uuid = '" + p3 + "'";
+	page->out("out", "<h2>Активизация аккаунта</h2>");
+	if (query->exec(sql)) {
+		page->out("out", "Ваш аккаунт был успешно активизирован...<br>\nВойдите на сайт в боковой панели сайта...");
+	}
+	else {
+		page->out("out", "Возникла ошибка при активизации аккаунта...");
+	}
+}
+void UserModule::changePassword(WebPage *page, HttpRequest &request) {
+	MySQL *query = manager->newQuery();
+	String uuid = request.header.COOKIE.getValue("uuid");
+	int userId = manager->getUserId(uuid);
+
+	WebTemplate * tpl = new WebTemplate();
+	String p3 = request.header.GET.getValue("p3");
+	if (p3 == "") {
+		if (userId != 0) {
+			if (tpl->open(manager->modulePath + "/2/changePassword_tpl.html")) {
+				tpl->exec();
+				page->out("out", tpl->html);
+			}
+		}
+		else {
+			if (tpl->open(manager->documentRoot + "/tpl/message_tpl.html")) {
+				tpl->out("caption", "Смена пароля");
+				tpl->out("error", "Для смены пароля войдите на сайт под своим логином и старым паролем");
+				tpl->exec();
+				page->out("out", tpl->html);
+			}
+		}
+	}
+	else if (p3 == "done") {
+		if (tpl->open(manager->documentRoot + "/tpl/message_tpl.html")) {
+			tpl->out("caption", "Смена пароля");
+
+			String message, error;
+
+			if (userId != 0) {
+				String oldPassword = request.header.POST.getValue("oldPassword");
+				String sql = "select * from users where id='" + (String)userId + "' and password='" + oldPassword + "'";
+				if (query->exec(sql)) {
+					if (query->storeResult()) {
+						int count = query->getRowCount();
+						if (count > 0) {
+							String newPassword = request.header.POST.getValue("newPassword");
+							String repeatPassword = request.header.POST.getValue("repeatPassword");
+							error = manager->isPasswordCorrect(newPassword);
+							if (error == "") {
+								if (newPassword == repeatPassword) {
+									String sql = "update users set password='" + newPassword + "' where id='" + userId + "'";
+									if (query->exec(sql)) {
+										message = "Пароль был успешно изменён!";
+									}
+									else {
+										error = "Ошибка базы данных";
+									}
+								}
+								else error = "Пароли не совпадают";
+							}
+						}
+						else error = "Старый пароль введён не корректно";
+					}
+				}
+			}
+			else {
+				error = "Для смены пароля войдите на сайт под своим логином и старым паролем";
+			}
+			tpl->out("message", message);
+			tpl->out("error", error);
+			tpl->exec();
+			page->out("out", tpl->html);
+		}
+	}
+}
+void UserModule::ajax(WebPage *page, HttpRequest &request) {
+	MySQL *query = manager->newQuery();
+	String obj = request.header.GET.getValue("p1");
+	String func = request.header.GET.getValue("p2");
+	String uuid = request.header.COOKIE.getValue("uuid");
+
+	if (obj == "user") {
+		if (func == "login") {
+			String login = request.header.POST.getValue("login");
+			String password = request.header.POST.getValue("password");
+			String chkSave = request.header.POST.getValue("chkSave");
+
+			page->tplIndex->out("out", "<note>\n");
+			page->tplIndex->out("out", "<login>" + login + "</login>\n");
+
+			String sql = (String)"select * from users where email='" + login + "' and password='" + password + "'";
+			if (query->exec(sql)) {
+				if (query->storeResult()) {
+					int count = query->getRowCount();
+					if (count > 0) {
+						String userId = query->getFieldValue(0, "id");
+						//						if (userId == 1) {
+						if (true) {
+							String sql = (String)"insert into uuid (uuid, userId, sec) values ('" + uuid + "', '" + userId + "', '10000')";
+							if (chkSave == "")
+								sql = (String)"insert into uuid (uuid, userId, sec) values ('" + uuid + "', '" + userId + "', '0')";
+							if (query->exec(sql)) {
+								page->tplIndex->out("out", "<result>" + ((String)1) + "</result>\n");
+							}
+
+							bool flag = false;
+							sql = "select serviceId, u.id from uuidPartner up, users u where isnull(up.deleted) and up.userId=u.id and cookie='" + uuid + "' order by u.id";
+							if (query->exec(sql)) {
+								if (query->storeResult()) {
+									int count = query->getRowCount();
+									if (count > 0) {
+										flag = true;
+										String ref = query->getFieldValue(0, "id");
+										String serviceId = query->getFieldValue(0, "serviceId");
+
+										sql = "select * from users where id='" + userId + "' and isnull(ref" + serviceId + ")";
+										if (query->exec(sql)) {
+											if (query->storeResult()) {
+												int count = query->getRowCount();
+												if (count > 0) {
+													//String ref1_1 = query->getFieldValue(0, "ref1_1");
+													sql = "update users set ref" + serviceId + "='" + ref + "' where id='" + userId + "'";
+													query->exec(sql);
+
+													for (int i = 1; i <= 4; i++) {
+														String si = i;
+														sql = "update users set ref" + serviceId + "_" + si + "=ref" + serviceId + "_" + si + "+1 where id='" + ref + "'";
+														query->exec(sql);
+
+														sql = "select * from users where id='" + ref + "' and ref1<>id";
+														if (query->exec(sql)) {
+															if (query->storeResult()) {
+																int count = query->getRowCount();
+																if (count > 0) {
+																	ref = query->getFieldValue(0, "ref" + serviceId);
+																}
+																else break;
+															}
+														}
+													}
+												}
+											}
+										}
+										sql = "update uuidPartner set deleted=1 where cookie='" + uuid + "'";
+										query->exec(sql);
+									}
+								}
+							}
+
+							if (flag == false) {
+								sql = "update users set ref1=0 where id='" + userId + "'";
+								query->exec(sql);
+							}
+
+						}
+						else {
+							page->tplIndex->out("out", "<error>Вход временно заблокирован - сайт в режиме разработки...</error>");
+						}
+					}
+				}
+			}
+		}
+		else if (func == "logout") {
+			/*
+			String login = request.header.POST.getValue("login");
+			if (login != "") {
+				int userId = getUserId(login);
+				String sql = (String)"delete from uuid where userId='" + (String)userId + "'";
+				page->tplIndex->out("out", "<login>" + login + "</login>\n");
+				if (query->exec(sql)) {
+					page->tplIndex->out("out", "<result>1</result>");
+				}
+			}
+			*/
+		}
+		page->tplIndex->out("out", "</note>\n");
+	}
+}
+
+}
---
+++ src/userModule.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "cj.h"
+#include "cjCms.h"
+
+namespace cj {
+
+class SiteManager;
+
+enum UserType { utNone, utAdmin, tuUser };
+
+class UserModule : public WebModule {
+public:
+	UserModule(SiteManager *manager);
+	String generateContent(WebPage *page, HttpRequest &request);
+
+	virtual void paint(WebPage *page, HttpRequest &request);
+	virtual void paintAbout(WebPage *page, HttpRequest &request);
+	virtual void paintRecovery(WebPage *page, HttpRequest &request);
+	virtual void paintSignup(WebPage *page, HttpRequest &request);
+
+	virtual void sendAccount(WebPage *page, HttpRequest &request);
+
+	virtual void activate(WebPage *page, HttpRequest &request);
+	virtual void changePassword(WebPage *page, HttpRequest &request);
+
+	virtual void ajax(WebPage *page, HttpRequest &request);
+};
+
+}
