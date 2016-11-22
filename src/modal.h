eac1a0c3c275c0fa8f1bad8febe3cd6108c77060
sitev <admin@sitev.ru>
2016-11-22 13:11:34 +0500

    added Modal & TaskModule

---
+++ src/modal.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "cjCms.h"
+
+namespace cj {
+
+class Modal {
+	String modalId;
+	WebTemplate *tplIndex;
+	String tag;
+	String caption; 
+	String content;
+	String buttons;
+public:
+	Modal(String modalId, WebTemplate *tpl, String tag);
+	virtual void setCaption(String caption);
+	virtual void setContent(String content);
+	virtual void setButtons(String buttons);
+	virtual void paint();
+};
+
+}
---
+++ src/taskModule.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "cj.h"
+#include "cjCms.h"
+
+namespace cj {
+
+class SiteManager;
+
+class TaskModule : public WebModule {
+	String uuid;
+	int userId;
+	int whoseTasks;
+public:
+	String p1, p2, p3;
+
+	TaskModule(SiteManager *manager);
+	virtual void paint(WebPage *page, HttpRequest &request);
+
+	virtual void paintTable(WebPage *page, HttpRequest &request);
+	virtual void paintWhoseTasks(WebTemplate *tpl, int userId);
+
+	//virtual void paintIndex(WebPage *page, HttpRequest &request);
+	//virtual void paintProjects(WebPage *page, HttpRequest &request);
+	//virtual void paintPriority(WebPage *page, HttpRequest &request);
+
+	virtual void paintEdit(WebPage *page, HttpRequest &request);
+	virtual void paintDetail(WebPage *page, HttpRequest &request, String p2);
+
+
+	virtual void paintSubTask(WebTemplate *tpl, int taskId, int level, bool isShowOnlyProject);
+	virtual void paintRow(String sql, WebTemplate *tpl, int level, bool isShowOnlyProject);
+	virtual void paintPriorityRow(String sql, WebTemplate *tpl, int level, bool isShowOnlyProject);
+
+	virtual void ajax(WebPage *page, HttpRequest &request);
+	virtual void ajaxAccept(WebPage *page, HttpRequest &request);
+	virtual void ajaxAdd(WebPage *page, HttpRequest &request);
+	virtual void ajaxDelete(WebPage *page, HttpRequest &request);
+	virtual void ajaxChange(WebPage *page, HttpRequest &request);
+	//	virtual void ajaxGetUrlByIndex(WebPage *page, HttpRequest &request);
+
+};
+
+}
